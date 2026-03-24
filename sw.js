a4a88a542c57ad76f490f3dbd2fc9ab32c50003a
anupamragh <anupamragh@proton.me>
2026-03-24 11:48:42 +0530

    SW: skipWaiting + clients.claim for instant activation

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-var CACHE = 'resizer-v3';
+var CACHE = 'resizer-v4';
 var URLS = ['.', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(URLS); }));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
@@ -12,7 +13,7 @@ self.addEventListener('activate', function(e) {
         names.filter(function(n) { return n !== CACHE; })
              .map(function(n) { return caches.delete(n); })
       );
-    })
+    }).then(function() { return self.clients.claim(); })
   );
 });
 
